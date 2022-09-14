80ce1d10f6510aaebcd98b86f154b93091d8c92b
Kodiak Drake <kodiakdrake@gmail.com>
2022-09-14 12:53:22 -0700

    Kodiak Drake 9/14/22
    Created methods to square functions

--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -1,8 +1,8 @@
 public class SimpleMethod {
-    // Put your method code here
-
+    public static int square(int retsq) {
+        return retsq*retsq;
+    }
     public static void main(String[] args) {
-        // This is the method call, create the square method.
-        square(25);
+        System.out.println(square(25));
     }
 }
